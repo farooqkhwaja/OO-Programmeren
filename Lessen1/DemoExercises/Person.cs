17ab0dc464cb8334f64ea1e654428501e7c9c3d1
farooq <farooqkhoja16@gmail.com>
2025-02-04 14:00:10 +0100

    first commit

---
+++ Lessen1/DemoExercises/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExercises
+{
+    internal class Person
+    {
+        Person person1 = new Person();
+        Random rnd = new Random();
+
+    }
+}
